6d0ca7abdbb087d3086b25e94f44510883093bd6
zyxwzyxw <149740824+zyxwzyxw@users.noreply.github.com>
2024-02-04 14:51:54 -0500

    a

--- src/views/index.vue
+++ src/views/index.vue
@@ -343,7 +343,7 @@ export default {
     },
 
     restoreFormData() {
-      this.formData.values(formData).forEach(input,index => {
+      this.formData.values(this.formData).forEach(input,index => {
         const formtype = this.form.index.type;
         const formotherValue = this.form.index.otherValue;
         try {
