3dc201f75d3dfc157c57366a98de779c64781282
1834423612 <1834423612@qq.com>
2024-02-26 18:30:07 -0500

    feat: tabs & team numbers

--- vite.config.js
+++ vite.config.js
@@ -6,8 +6,7 @@ import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	publicPath: './',
-	base: "/",
+	base: "/pit-scouting/",
 	server: {
 		port: 8080
 	},
